f18f68df7297de39165111175f2f879e8d24eda9
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2024-01-07 23:44:28 +0100

    new builds and total refractor

---
+++ src/stories/Styro.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { Styro } from '../components/styro'
+
+const stories = storiesOf('App test', module)
+
+stories.add('App', () => {
+  return <Styro />
+})
